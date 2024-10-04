3421b483d55b0db27f3dcb29007909d2fbb3fbf1
oliviaSkyeHolley <o.holley.aus@gmail.com>
2024-10-04 16:24:44 +1000

    ...

---
+++ custom/decision_support/src/Plugin/rest/resource/GetDecisionSupportReportListResource.php
@@ -0,0 +1,169 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Drupal\decision_support\Plugin\rest\resource;
+
+use Drupal\Core\KeyValueStore\KeyValueFactoryInterface;
+use Drupal\Core\KeyValueStore\KeyValueStoreInterface;
+use Drupal\Core\Session\AccountProxyInterface;
+use Drupal\decision_support\Services\DecisionSupport\DecisionSupportService;
+use Drupal\rest\ModifiedResourceResponse;
+use Drupal\rest\Plugin\ResourceBase;
+use Drupal\rest\ResourceResponse;
+use Psr\Log\LoggerInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\Routing\Route;
+use Drupal\decision_support\Entity\DecisionSupport;
+
+/**
+ * Represents get_decision_support_report_list records as resources.
+ *
+ * @RestResource (
+ *   id = "get_decision_support_report_list_resource",
+ *   label = @Translation("get_decision_support_report_list"),
+ *   uri_paths = {
+ *     "canonical" = "/api/get-decision-support-report-list-resource/{id}",
+ *     "create" = "/api/get-decision-support-report-list-resource"
+ *   }
+ * )
+ *
+ * @DCG
+ * The plugin exposes key-value records as REST resources. In order to enable it
+ * import the resource configuration into active configuration storage. An
+ * example of such configuration can be located in the following file:
+ * core/modules/rest/config/optional/rest.resource.entity.node.yml.
+ * Alternatively, you can enable it through admin interface provider by REST UI
+ * module.
+ * @see https://www.drupal.org/project/restui
+ *
+ * @DCG
+ * Notice that this plugin does not provide any validation for the data.
+ * Consider creating custom normalizer to validate and normalize the incoming
+ * data. It can be enabled in the plugin definition as follows.
+ * @code
+ *   serialization_class = "Drupal\foo\MyDataStructure",
+ * @endcode
+ *
+ * @DCG
+ * For entities, it is recommended to use REST resource plugin provided by
+ * Drupal core.
+ * @see \Drupal\rest\Plugin\rest\resource\EntityResource
+ */
+final class GetDecisionSupportReportListResource extends ResourceBase {
+
+  /**
+   * The key-value storage.
+   */
+  private readonly KeyValueStoreInterface $storage;
+
+  /**
+   * {@inheritdoc}
+   */
+  public function __construct(
+    array $configuration,
+    $plugin_id,
+    $plugin_definition,
+    array $serializer_formats,
+    LoggerInterface $logger,
+    KeyValueFactoryInterface $keyValueFactory,
+    AccountProxyInterface $currentUser,
+    DecisionSupportService $decision_support_service
+  ) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition, $serializer_formats, $logger);
+    $this->storage = $keyValueFactory->get('get_decision_support_report_list_resource');
+    $this->currentUser = $currentUser;
+    $this->decisonSupportService = $decision_support_service;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition): self {
+    return new self(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $container->getParameter('serializer.formats'),
+      $container->get('logger.factory')->get('rest'),
+      $container->get('keyvalue'),
+      $container->get('current_user'),
+      $container->get('decision_support.service')
+    );
+  }
+
+
+  /**
+   * Responds to GET requests.
+   *
+   * @return \Drupal\rest\ResourceResponse
+   *  The HTTP response object.
+   *
+   * public function get($id): ResourceResponse {
+   *
+   * // Check user permissions.
+   * if (!$this->currentUser->hasPermission('access content')) {
+   * throw new AccessDeniedHttpException();
+   * }
+   */
+  public function get()
+  {
+
+    // Check user permissions.
+    if (!$this->currentUser->hasPermission('access content')) {
+      throw new AccessDeniedHttpException();
+    }
+
+    try {
+      // Retrieve the list of decision.
+      $decisionSupportReportList = $this->decisionSupportService->getdecisionSupportReportList();
+      $response = new ResourceResponse($decisionSupportReportList);
+      $response->addCacheableDependency($this->currentUser);
+
+      return $response;
+    }
+    catch (\Exception $e) {
+      // Log the error message.
+      $this->logger->error('An error occurred while loading decision support list: @message', ['@message' => $e->getMessage()]);
+
+      // Throw a generic HTTP exception for internal server errors.
+      throw new HttpException(500, 'Internal Server Error');
+    }
+  }
+}
+/*
+    if (!$this->storage->has($id)) {
+      throw new NotFoundHttpException();
+    }
+    $resource = $this->storage->get($id);
+    return new ResourceResponse($resource);
+  }
+*/
+/*
+
+  /**
+   * {@inheritdoc}
+   */
+
+/*
+  protected function getBaseRoute($canonical_path, $method): Route {
+    $route = parent::getBaseRoute($canonical_path, $method);
+    // Set ID validation pattern.
+    if ($method !== 'POST') {
+      $route->setRequirement('id', '\d+');
+    }
+    return $route;
+  }
+
+  /**
+   * Returns next available ID.
+   */
+/*
+  private function getNextId(): int {
+    $ids = \array_keys($this->storage->getAll());
+    return count($ids) > 0 ? max($ids) + 1 : 1;
+  }
+
+}
+ */
